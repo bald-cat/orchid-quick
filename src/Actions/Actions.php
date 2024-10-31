95fa36f82fae1b4441cfe94d9227dd5d904113d5
bald-cat <babaiev.ihor@gmail.com>
2024-10-31 16:28:59 +0200

    Changed Actions.php

--- src/Actions/Actions.php
+++ src/Actions/Actions.php
@@ -12,18 +12,33 @@ class Actions
     /**
      * Return Actions button group for table
      * @param array $actions
+     * @param array|null $customActions
      * @return TD
      */
-    public static function make(array $actions = []): TD
+    public static function make(array $actions = [], ?array $customActions = null): TD
     {
-        return TD::make('Actions')->render(function ($row) use ($actions) {
+        $count = count($actions);
+        if ($customActions) {
+            $count += count($customActions);
+        }
+
+        $width = $count * 100;
+
+        return TD::make('Actions')->render(function ($row) use ($actions, $customActions) {
 
             $group = [];
+
+            if ($customActions) {
+                foreach ($customActions as $action) {
+                    $group[] = $action;
+                }
+            }
+
             foreach ($actions as $action) {
                 $group[] = $action::make($row);
             }
 
             return Group::make($group)->toEnd();
-        })->width(count($actions) * 75);
+        })->width($width);
     }
 }
